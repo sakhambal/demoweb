f63389e4b00525449df5b6268ec71c105883b4ff
Sahil Anil Khambal <sahilakhambal@gmail.com>
2026-07-25 20:39:59 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ console.log(logo)
 
 const NAV_LINKS = [
   { label: "Home", href: "#home" },
-  { label: "About", href: "#about" },
+  { label: "About", href: "#teams" },
   { label: "Services", href: "#services" },
   { label: "Contact", href: "#contact" },
 ];
